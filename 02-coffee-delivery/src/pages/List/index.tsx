32d715fd740f890b46e2e1810cd19aef45b780a7
Felipe Scherer <felipescherer25@gmail.com>
2023-06-14 13:06:16 -0300

    :sparkles: feat: header stats

--- 02-coffee-delivery/src/pages/List/index.tsx
+++ 02-coffee-delivery/src/pages/List/index.tsx
@@ -1,15 +1,48 @@
+import { Clock, Coffee, Package, ShoppingCart } from '@phosphor-icons/react'
 import * as S from './styles'
 
 export function List() {
   return (
     <S.Wrapper>
-      <S.TitleContainer>
-        <S.Title>Encontre o café perfeito para qualquer hora do dia</S.Title>
-        <S.Subtitle>
-          Com o Coffee Delivery você recebe seu café onde estiver, a qualquer
-          hora
-        </S.Subtitle>
-      </S.TitleContainer>
+      <S.TitleWrapper>
+        <S.TitleContainer>
+          <S.Title>Encontre o café perfeito para qualquer hora do dia</S.Title>
+          <S.Subtitle>
+            Com o Coffee Delivery você recebe seu café onde estiver, a qualquer
+            hora
+          </S.Subtitle>
+        </S.TitleContainer>
+
+        <S.StatsContainer>
+          <S.StatItem>
+            <S.Icon bg="yellow-dark">
+              <ShoppingCart />
+            </S.Icon>
+            Compra simples e segura
+          </S.StatItem>
+
+          <S.StatItem>
+            <S.Icon bg="base-text">
+              <Package />
+            </S.Icon>
+            Embalagem mantém o café intacto
+          </S.StatItem>
+
+          <S.StatItem>
+            <S.Icon bg="yellow">
+              <Clock />
+            </S.Icon>
+            Entrega rápida e rastreada
+          </S.StatItem>
+
+          <S.StatItem>
+            <S.Icon bg="purple">
+              <Coffee />
+            </S.Icon>
+            O Café chega fresquinho até você
+          </S.StatItem>
+        </S.StatsContainer>
+      </S.TitleWrapper>
       <S.imgDiv></S.imgDiv>
     </S.Wrapper>
   )
--- 02-coffee-delivery/src/pages/List/styles.ts
+++ 02-coffee-delivery/src/pages/List/styles.ts
@@ -1,8 +1,18 @@
 import styled from 'styled-components'
 
+type IconType = {
+  bg: 'yellow-dark' | 'yellow' | 'base-text' | 'purple'
+}
+
 export const Wrapper = styled.div`
   display: flex;
   justify-content: space-between;
+`
+
+export const TitleWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
 `
 
 export const TitleContainer = styled.div`
@@ -21,6 +31,27 @@ export const Title = styled.h1`
 export const Subtitle = styled.div`
   font-size: ${({ theme }) => theme.typography.text.l};
   color: ${({ theme }) => theme.colors['base-subtitle']};
+`
+
+export const StatsContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 1rem;
+`
+
+export const StatItem = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+`
+
+export const Icon = styled.div<IconType>`
+  padding: 0.5rem;
+  border-radius: 50%;
+  width: 32px;
+  height: 32px;
+  color: ${({ theme }) => theme.colors.white};
+  background-color: ${({ bg, theme }) => theme.colors[bg]};
 `
 
 export const imgDiv = styled.div`
