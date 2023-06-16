ddad5a8568974b3f701ab25f7ebd5045483157f5
Felipe Scherer <felipescherer25@gmail.com>
2023-06-16 13:18:08 -0300

    :sparkles: feat: shopping items list

--- 02-coffee-delivery/src/components/SelectedCoffee.tsx/index.tsx
+++ 02-coffee-delivery/src/components/SelectedCoffee.tsx/index.tsx
@@ -1,7 +1,24 @@
+import { CoffeeList } from '../../@types/Coffee'
+import { useShoppingContext } from '../../context/shopping'
 import { CoffeeShoppingList } from '../CoffeeShoppingList'
 import * as S from './styles'
 
+function getTotalItens(coffeeList: CoffeeList[]) {
+  let total = 0
+
+  coffeeList.map((item) => (total += item.quantity * item.coffee.price))
+
+  return total
+}
+
 export function SelectedCoffee() {
+  const { coffeeList } = useShoppingContext()
+  const delivery = 3
+
+  if (!coffeeList) return <></>
+
+  const totalItens = getTotalItens(coffeeList)
+
   return (
     <S.Wrapper>
       <CoffeeShoppingList />
@@ -9,17 +26,17 @@ export function SelectedCoffee() {
       <S.TotalItensContainer>
         <S.Item>
           <span>Total de Itens</span>
-          <span>R$ {'29,18'}</span>
+          <span>R$ {totalItens.toFixed(2)}</span>
         </S.Item>
 
         <S.Item>
           <span>Entrega</span>
-          <span>R$ {'29,18'}</span>
+          <span>R$ {delivery.toFixed(2)}</span>
         </S.Item>
 
         <S.TotalItem>
           <span>Total</span>
-          <span>R$ {'29,18'}</span>
+          <span>R$ {(totalItens + delivery).toFixed(2)}</span>
         </S.TotalItem>
       </S.TotalItensContainer>
 
