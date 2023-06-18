cd6eba2b55360f6877f504f5ea038f2dc5cb003b
Felipe Scherer <felipescherer25@gmail.com>
2023-06-18 15:18:17 -0300

    :sparkles: feat: payment

--- 02-coffee-delivery/src/components/PaymentForm/index.tsx
+++ 02-coffee-delivery/src/components/PaymentForm/index.tsx
@@ -1,8 +1,29 @@
-import { Money } from '@phosphor-icons/react'
+import { Bank, CreditCard, Money } from '@phosphor-icons/react'
 import { FormHeader } from '../FormHeader'
 import * as S from './styles'
+import { useState } from 'react'
+
+const paymentMethods = [
+  {
+    id: 1,
+    icon: <CreditCard />,
+    title: 'Cartão de crédito'
+  },
+  {
+    id: 2,
+    icon: <Bank />,
+    title: 'Cartão de crédito'
+  },
+  {
+    id: 3,
+    icon: <Money />,
+    title: 'Cartão de crédito'
+  }
+]
 
 export function PaymentForm() {
+  const [isSelected, setIsSelected] = useState(0)
+
   return (
     <S.Wrapper>
       <FormHeader
@@ -11,6 +32,19 @@ export function PaymentForm() {
         title="Pagamento"
         description="O pagamento é feito na entrega. Escolha a forma que deseja pagar"
       />
+
+      <S.PaymentContainer>
+        {paymentMethods.map((item) => (
+          <S.ItemContainer
+            key={item.id}
+            isSelected={isSelected === item.id}
+            onClick={() => setIsSelected(item.id)}
+          >
+            {item.icon}
+            <S.Item>{item.title}</S.Item>
+          </S.ItemContainer>
+        ))}
+      </S.PaymentContainer>
     </S.Wrapper>
   )
 }
--- 02-coffee-delivery/src/components/PaymentForm/styles.ts
+++ 02-coffee-delivery/src/components/PaymentForm/styles.ts
@@ -4,4 +4,36 @@ export const Wrapper = styled.div`
   background-color: ${({ theme }) => theme.colors['base-card']};
   padding: 1rem;
   border-radius: 6px;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`
+
+export const PaymentContainer = styled.div`
+  display: flex;
+  gap: 0.5rem;
+`
+
+export const ItemContainer = styled.button<{ isSelected: boolean }>`
+  display: flex;
+  gap: 0.5rem;
+  color: ${({ theme }) => theme.colors['purple']};
+  background-color: ${({ theme, isSelected }) =>
+    isSelected ? theme.colors['purple-light'] : theme.colors['base-button']};
+  border-radius: 6px;
+  flex: 1;
+  padding: 1rem;
+  border: 1px solid
+    ${({ theme, isSelected }) =>
+      isSelected ? theme.colors['purple-dark'] : 'transparent'};
+  transition: 0.2s;
+  cursor: pointer;
+
+  &:hover {
+    border-color: ${({ theme }) => theme.colors['purple-dark']};
+  }
+`
+
+export const Item = styled.span`
+  color: ${({ theme }) => theme.colors['base-text']};
 `
