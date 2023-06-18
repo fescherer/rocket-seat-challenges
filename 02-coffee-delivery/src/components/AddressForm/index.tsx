c52056830bcaa72e6b3c53d0a81f4229440912c9
Felipe Scherer <felipescherer25@gmail.com>
2023-06-18 14:59:08 -0300

    :sparkles: feat: form address

--- 02-coffee-delivery/src/components/AddressForm/index.tsx
+++ 02-coffee-delivery/src/components/AddressForm/index.tsx
@@ -1,9 +1,33 @@
+import { MapPinLine } from '@phosphor-icons/react'
 import * as S from './styles'
 
 export function AddressForm() {
   return (
     <S.Wrapper>
-      <span>Address Form</span>
+      <S.Header>
+        <MapPinLine size={24} />
+        <S.TitleContainer>
+          <S.Title>Endereço de entrega</S.Title>
+          <S.Description>
+            Informe o endereço onde deseja receber seu pedido
+          </S.Description>
+        </S.TitleContainer>
+      </S.Header>
+
+      <S.Container>
+        <input placeholder="tel" />
+        <input placeholder="Rua" />
+        <S.NumberContainer>
+          <input placeholder="Número" />
+          <input placeholder="Complemento" />
+        </S.NumberContainer>
+
+        <S.AdressContainer>
+          <input placeholder="Bairro" />
+          <input placeholder="Cidade" />
+          <input placeholder="RS" />
+        </S.AdressContainer>
+      </S.Container>
     </S.Wrapper>
   )
 }
--- 02-coffee-delivery/src/components/AddressForm/styles.ts
+++ 02-coffee-delivery/src/components/AddressForm/styles.ts
@@ -5,3 +5,40 @@ export const Wrapper = styled.div`
   padding: 1rem;
   border-radius: 6px;
 `
+
+export const Header = styled.div`
+  display: flex;
+  gap: 0.5rem;
+  color: ${({ theme }) => theme.colors['yellow-dark']};
+`
+export const TitleContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+export const Title = styled.h2`
+  font-size: ${({ theme }) => theme.typography.text.m};
+  font-weight: ${({ theme }) => theme.fontWeight.bold};
+  color: ${({ theme }) => theme.colors['base-subtitle']};
+`
+
+export const Description = styled.span`
+  font-size: ${({ theme }) => theme.typography.text.s};
+  font-weight: ${({ theme }) => theme.fontWeight.regular};
+  color: ${({ theme }) => theme.colors['base-text']};
+`
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+export const NumberContainer = styled.div`
+  display: flex;
+  gap: 0.5rem;
+`
+
+export const AdressContainer = styled.div`
+  display: flex;
+  gap: 0.5rem;
+`
