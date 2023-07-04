9beb68e8a6e868f764300d90a84242b0b916475f
Felipe Scherer <felipescherer25@gmail.com>
2023-07-04 12:57:27 -0300

    :sparkles: style head

--- 03-github-blog/src/components/Header/index.tsx
+++ 03-github-blog/src/components/Header/index.tsx
@@ -1,9 +1,14 @@
 import * as S from './styles'
+import effect from '../../assets/effect.svg'
+import effect2 from '../../assets/effect2.svg'
+import logo from '../../assets/logo.svg'
 
 export function Header() {
   return (
     <S.Background>
-      <span>Header</span>
+      <S.ImgEffect src={effect} style={{ left: 0 }} />
+      <img src={logo} />
+      <S.ImgEffect src={effect2} style={{ right: 0 }} />
     </S.Background>
   )
 }
--- 03-github-blog/src/components/Header/styles.ts
+++ 03-github-blog/src/components/Header/styles.ts
@@ -3,4 +3,15 @@ import { styled } from 'styled-components'
 export const Background = styled.div`
   background-color: ${({ theme }) => theme.colors['base-background']};
   color: ${({ theme }) => theme.colors['base-text']};
+  position: relative;
+  height: 100%;
+  min-height: 18rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+export const ImgEffect = styled.img`
+  position: absolute;
 `
