7349704d5f8b23e7c70c009449204dc4ae66f370
Felipe Scherer <felipescherer25@gmail.com>
2023-10-01 16:23:57 -0300

    :sparkles: remove project 4

--- 04-ignite-shop/src/contexts/index.ts
+++
@@ -1,7 +0,0 @@
-import { useContext } from "react";
-import { ShoppingCarToggleContext } from "./ShoppingCarToggle.context.";
-import { ShoppingCarContext } from "./ShoppingCar.context";
-
-export const useShoppingCarToggleContext = () => useContext(ShoppingCarToggleContext)
-
-export const useShoppingCarContext = () => useContext(ShoppingCarContext)
